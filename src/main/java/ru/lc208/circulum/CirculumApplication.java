dd48dc13d09dd23de851fa3d6902d5b5a3e3706f
LC208 <maks.2.03@mail.ru>
2024-11-28 21:42:42 +0800

    change entry point

--- src/main/java/ru/lc208/circulum/CirculumApplication.java
+++ src/main/java/ru/lc208/circulum/CirculumApplication.java
@@ -2,18 +2,19 @@ package ru.lc208.circulum;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class CirculumApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("connection.fxml"));
+        Parent p = loader.load();
+        stage.setScene(new Scene(p));
+        stage.setTitle("Database Connection");
         stage.show();
     }
 
