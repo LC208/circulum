ebe5fd12bed5c81b1a16ef07944d1687212e3d59
LC208 <maks.2.03@mail.ru>
2024-11-28 21:44:13 +0800

    add entities from db, ui description in fxml, config files for hibernate

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module ru.lc208.circulum {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.naming;
+    requires javafx.base;
 
     opens ru.lc208.circulum to javafx.fxml;
+    opens ru.lc208.circulum.controllers to javafx.fxml;
+    opens ru.lc208.circulum.entities to org.hibernate.orm.core;
+    exports ru.lc208.circulum.controllers to javafx.fxml;
     exports ru.lc208.circulum;
 }
